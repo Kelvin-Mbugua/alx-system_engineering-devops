5328830c6acdbb5a86c23691473e3ebaa3810cf3
“Kelvin-Mbugua” <kmbugua.m@gmail.com>
2023-04-19 15:52:38 -0700

    MYSQL

--- 0x14-mysql/create_user.sql
+++ 0x14-mysql/create_user.sql
@@ -1,6 +1,6 @@
 -- Create user holberton and give access to primary/replica status of the database
 CREATE USER
- IF NOT EXISTS 'holberton_user'@'localhost' 
+ IF NOT EXISTS 'holberton_user'@'54.237.2.30''52.86.27.254' 
  IDENTIFIED BY 'projectcorrection280hbtn';
 GRANT SELECT,INSERT,UPDATE, REPLICATION CLIENT, REPLICATION SLAVE
   ON * . * 
